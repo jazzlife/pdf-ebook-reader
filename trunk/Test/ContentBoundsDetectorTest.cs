e5499ad8efb3078246a63559c656793267eeabe8
danilom <danilom@c21f06b1-db24-b691-a423-4f2667294ccf>
2011-05-31 15:46:15 +0000

    Reorganizing code for perf testing.

--- trunk/Test/ContentBoundsDetectorTest.cs
+++ trunk/Test/ContentBoundsDetectorTest.cs
@@ -27,13 +27,43 @@ namespace PDFViewer.Test
         [Test]
         public void AForgeEdgeDetect()
         {
-            ProcesAllImages("AForge", ContentBoundsDetector.RenderBlobsInfo);
+            _timerDetectBlobs = new PerfTimer("DetectBlobs");
+            _timerMainContent = new PerfTimer("MainContentBounds");
+            _timerRows = new PerfTimer("RowBounds");
+
+            ProcesAllImages(ProcessOneImage);
+
+            Console.WriteLine();
+            Console.WriteLine(_timerDetectBlobs);
+            Console.WriteLine(_timerMainContent);
+            Console.WriteLine(_timerRows);
         }
 
-        void ProcesAllImages(String timerName, CustomRenderDelegate fnRender, int numFiles = -1)
-        {
-            PerfTimer timer = new PerfTimer(timerName);
+        PerfTimer _timerDetectBlobs;
+        PerfTimer _timerMainContent;
+        PerfTimer _timerRows;
 
+        void ProcessOneImage(Bitmap bmp, Graphics g)
+        {
+            ContentBoundsInfo cbi;
+            ContentBoundsDetector detector = new ContentBoundsDetector();
+
+            using (_timerDetectBlobs.NewRun)
+            {
+                cbi = detector.DetectBlobs(bmp, g);
+            }
+            using (_timerMainContent.NewRun)
+            {
+                detector.DetectMainContentBounds(ref cbi, g);
+            }
+            using (_timerRows.NewRun)
+            {
+                detector.DetectRowBounds(ref cbi, g);
+            }
+        }
+
+        void ProcesAllImages(CustomRenderDelegate fnRender, int numFiles = -1)
+        {
             var files = GetImageFiles().Select(x => x);
             if (numFiles > 0) { files = files.Take(numFiles); }
 
@@ -48,19 +78,13 @@ namespace PDFViewer.Test
                         {
                             g.DrawImageUnscaled(inBmp, 0, 0);
 
-                            using (timer.NewRun)
-                            {
-                                fnRender(outBmp, g);
-                            }
+                            fnRender(outBmp, g);
 
                             outBmp.Save(String.Format(@"C:\temp\out{0:000}.jpg", i++), ImageFormat.Png);
                         }
                     }
                 }
             }
-
-            Console.WriteLine();
-            Console.WriteLine(timer);
         }
 
     }
