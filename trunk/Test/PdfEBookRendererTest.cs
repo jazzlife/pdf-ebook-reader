c6f8d8d4936973c6d1c16543fefe81e2f26cb7da
danilom <danilom@c21f06b1-db24-b691-a423-4f2667294ccf>
2011-05-31 07:06:55 +0000

    Test: perf timer Renderer: quality option

--- trunk/Test/PdfEBookRendererTest.cs
+++ trunk/Test/PdfEBookRendererTest.cs
@@ -7,6 +7,8 @@ using System.Drawing;
 using PDFViewer.Reader.Utils;
 using PDFViewer.Reader;
 using System.Drawing.Imaging;
+using PDFViewer.Test.TestUtils;
+using System.IO;
 
 namespace Test
 {
@@ -40,23 +42,53 @@ namespace Test
             Assert.AreEqual(result, real.ScaleToFitBounds(limit));
         }
 
+
+        String file1 = @"..\..\..\SampleTestPDFs\Pictures - Predictably Irrational.pdf";
+        String file2 = @"..\..\..\SampleTestPDFs\Bad Scan Tilted Facing Pages Big - Solzhenitsyn.pdf";
+        String file3 = @"..\..\..\SampleTestPDFs\Clean Tex - Hitchhiker's.pdf";
+
         [Test]
-        public void GeneratePages()
+        public void GeneratePages1_Fast() { GeneratePages(file1, RenderQuality.Fast); }
+        [Test]
+        public void GeneratePages1_Quality() { GeneratePages(file1, RenderQuality.HighQualityMuPdf); }
+
+        [Test]
+        public void GeneratePages2_Fast() { GeneratePages(file2, RenderQuality.Fast); }
+        [Test]
+        public void GeneratePages2_Quality() { GeneratePages(file2, RenderQuality.HighQualityMuPdf); }
+
+        [Test]
+        public void GeneratePages3_Fast() { GeneratePages(file3, RenderQuality.Fast); }
+        [Test]
+        public void GeneratePages3_Quality() { GeneratePages(file3, RenderQuality.HighQualityMuPdf); }
+
+        void GeneratePages(String file, RenderQuality quality)
         {
-            String file = @"..\..\..\SampleTestPDFs\Different Page Sizes - Dawkins God.pdf";
-
             PdfEBookRenderer r = new PdfEBookRenderer();
-            r.LoadPdf(file);
 
-            for (int pageNum = 1; pageNum < 15; pageNum++)
+            using (PerfTimer t = new PerfTimer("Loading " + Path.GetFileName(file)))
             {
-                using (Bitmap bmp = r.RenderPdfPageToBitmap(new Size(600, 600), pageNum))
+                r.LoadPdf(file);
+            }
+
+            int numPages = Math.Min(30, r.PageCount);
+
+            using(PerfTimer t = new PerfTimer("Rendering pages " + quality, numPages))
+            {
+                for (int pageNum = 1; pageNum <= numPages; pageNum++)
                 {
-                        String imgFile = @"C:\temp\page" + pageNum + ".png";
-                        bmp.Save(imgFile, ImageFormat.Png);
+                    Console.Write(".");
+                    using (Bitmap bmp = r.RenderPdfPageToBitmap(pageNum, new Size(1000, 1000), quality))
+                    {
+                            String imgFile = String.Format(@"C:\temp\page{0:000}_{1}.png", pageNum, quality);
+                            //bmp.Save(imgFile, ImageFormat.Png);
+                    }
                 }
+                Console.WriteLine();
             }
 
         }
+
     }
+
 }
---
+++ trunk/Test/TestUtils/PerfTimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PDFViewer.Test.TestUtils
+{
+    public class PerfTimer : IDisposable
+    {
+        readonly DateTime StartTime;
+        readonly String Name;
+        readonly int NumOps;
+
+        public PerfTimer(String name, int numOps = 1)
+        {
+            if (numOps < 1) { throw new ArgumentException("numOps < 1"); }
+
+            StartTime = DateTime.Now;
+            Name = name;
+            NumOps = numOps;
+        }
+
+        public void Dispose()
+        {
+            DateTime endTime = DateTime.Now;
+            TimeSpan duration = endTime.Subtract(StartTime);
+
+            Console.WriteLine(Name + " " + (duration.TotalMilliseconds / NumOps) + " ms");
+        }
+    }
+}
