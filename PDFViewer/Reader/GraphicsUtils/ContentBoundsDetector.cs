e592e2361048293aec99c5fbf9e802503a7cddce
danilom <danilom@c21f06b1-db24-b691-a423-4f2667294ccf>
2011-05-31 15:46:15 +0000

    Reorganizing code for perf testing.

--- PDFViewer/Reader/GraphicsUtils/ContentBoundsDetector.cs
+++ PDFViewer/Reader/GraphicsUtils/ContentBoundsDetector.cs
@@ -9,14 +9,27 @@ using AForge.Imaging.Filters;
 
 namespace PDFViewer.Reader.GraphicsUtils
 {
+    public class ContentBoundsInfo
+    {
+        public Blob[] Blobs;
+        public Rectangle Bounds;
+
+        public List<Rectangle> Rows;
+
+        public Rectangle Header;
+        public Rectangle Footer;
+    }
+
     /// <summary>
     /// Detect real bounds of the content on a physical page
     /// (main text, header, footer).
     /// </summary>
     public class ContentBoundsDetector
     {
-        public static void RenderBlobsInfo(Bitmap bmp, Graphics g)
+        internal ContentBoundsInfo DetectBlobs(Bitmap bmp, Graphics g)
         {
+            ContentBoundsInfo cbi = new ContentBoundsInfo();
+
             Invert filter = new Invert();
             filter.ApplyInPlace(bmp);
 
@@ -28,28 +41,57 @@ namespace PDFViewer.Reader.GraphicsUtils
 
             bc.ProcessImage(bmp);
 
-            var blobs = bc.GetObjectsInformation();
-            if (bc.ObjectsCount == 0) 
+            cbi.Blobs = bc.GetObjectsInformation();
+            
+            // Debug
+            if (bc.ObjectsCount == 0)
             {
-                g.FillEllipse(Brushes.Red, 0,0,10,10);
-                return;
+                g.FillEllipse(Brushes.Red, 0, 0, 10, 10);
+            }
+            else
+            {
+                foreach (var blob in cbi.Blobs)
+                {
+                    g.DrawRectangle(Pens.Orange, blob.Rectangle);
+                }
+            }
+            return cbi;
+        }
+
+        internal void DetectMainContentBounds(ref ContentBoundsInfo cbi, Graphics g)
+        {
+            if (cbi.Blobs == null) { throw new ArgumentException("cbi.Blobs is null"); }
+            if (cbi.Blobs.Length == 0) { return; }
+
+            int left = cbi.Blobs.Select(b => b.Rectangle.Left).Min();
+            int right = cbi.Blobs.Select(b => b.Rectangle.Right).Max();
+            int top = cbi.Blobs.Select(b => b.Rectangle.Top).Min();
+            int bottom = cbi.Blobs.Select(b => b.Rectangle.Bottom).Max();
+
+            cbi.Bounds = new Rectangle(left, top, right - left, bottom - top);
+            g.DrawRectangle(Pens.Cyan, cbi.Bounds);
+        }
+
+        internal void DetectRowBounds(ref ContentBoundsInfo cbi, Graphics g)
+        {
+            if (cbi.Blobs == null) { throw new ArgumentException("cbi.Blobs is null"); }
+            if (cbi.Blobs.Length == 0) { return; }
+            if (cbi.Bounds == Rectangle.Empty) { return; }
+
+            cbi.Rows = new List<Rectangle>();
+
+            // Attempt drawing lines between the rows.
+            for (int y = cbi.Bounds.Top; y < cbi.Bounds.Bottom; y++)
+            {
+                Rectangle rowRect = new Rectangle(cbi.Bounds.Left, y, cbi.Bounds.Width, 1);
+                if (cbi.Blobs.FirstOrDefault(b => b.Rectangle.IntersectsWith(rowRect)) == null)
+                {
+                    g.FillRectangle(Brushes.LightGreen, rowRect);
+                }
             }
 
-            foreach (var blob in blobs)
-            {
-                if (blob.Rectangle.Size == bmp.Size) { continue; }
-
-                g.DrawRectangle(Pens.Orange, blob.Rectangle);
-            }
-
-            int left = blobs.Select(b => b.Rectangle.Left).Min();
-            int right = blobs.Select(b => b.Rectangle.Right).Max();
-            int top = blobs.Select(b => b.Rectangle.Top).Min();
-            int bottom = blobs.Select(b => b.Rectangle.Bottom).Max();
-
-            Rectangle contentBounds = new Rectangle(left, top, right - left, bottom - top);
-
-            g.DrawRectangle(Pens.Cyan, contentBounds);
+            // TODO: optimize
+            // TODO: add actual rows (not just 1-pixel thin rectangles)
         }
 
         class BlobsFilter : IBlobsFilter
