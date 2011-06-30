d2c3df0171827b82a4a7b0f70e4b3b98146f6136
danilom <danilom@c21f06b1-db24-b691-a423-4f2667294ccf>
2011-06-30 10:33:55 +0000

    UI for evaluating layout algorithms

--- Test/Render/Layout/LayoutForm.Designer.cs
+++ Test/Render/Layout/LayoutForm.Designer.cs
@@ -29,65 +29,222 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LayoutForm));
+            this.bPass = new System.Windows.Forms.ToolStripButton();
+            this.bIgnore = new System.Windows.Forms.ToolStripButton();
+            this.bClearData = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.bPassAcceptable = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator7 = new System.Windows.Forms.ToolStripSeparator();
+            this.tbComment = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
+            this.bFail = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.bHaltTests = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.lbStatus = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.lbFilename = new System.Windows.Forms.ToolStripLabel();
+            this.lbPageNum = new System.Windows.Forms.ToolStripLabel();
+            this.pbPage = new System.Windows.Forms.PictureBox();
             this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPage)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bPass
+            // 
+            this.bPass.BackColor = System.Drawing.Color.Lime;
+            this.bPass.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bPass.Image = ((System.Drawing.Image)(resources.GetObject("bPass.Image")));
+            this.bPass.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bPass.Name = "bPass";
+            this.bPass.Size = new System.Drawing.Size(110, 28);
+            this.bPass.Text = "  Good  ";
+            this.bPass.Click += new System.EventHandler(this.bCorrect_Click);
+            // 
+            // bIgnore
+            // 
+            this.bIgnore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.bIgnore.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bIgnore.Image = ((System.Drawing.Image)(resources.GetObject("bIgnore.Image")));
+            this.bIgnore.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bIgnore.Name = "bIgnore";
+            this.bIgnore.Size = new System.Drawing.Size(110, 28);
+            this.bIgnore.Text = " Ignore ";
+            this.bIgnore.Click += new System.EventHandler(this.bIgnore_Click);
+            // 
+            // bClearData
+            // 
+            this.bClearData.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bClearData.Image = ((System.Drawing.Image)(resources.GetObject("bClearData.Image")));
+            this.bClearData.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bClearData.Name = "bClearData";
+            this.bClearData.Size = new System.Drawing.Size(122, 28);
+            this.bClearData.Text = "  Clear  ";
+            this.bClearData.Click += new System.EventHandler(this.bClearData_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
             // 
             // toolStrip1
             // 
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton2,
-            this.toolStripButton3,
-            this.toolStripButton1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 700);
+            this.bPass,
+            this.toolStripSeparator1,
+            this.bPassAcceptable,
+            this.toolStripSeparator7,
+            this.tbComment,
+            this.toolStripSeparator6,
+            this.bFail,
+            this.toolStripSeparator3,
+            this.bIgnore,
+            this.bHaltTests,
+            this.toolStripSeparator5,
+            this.bClearData,
+            this.toolStripSeparator4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 790);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(635, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(930, 31);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // toolStripButton1
+            // bPassAcceptable
             // 
-            this.toolStripButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(79, 22);
-            this.toolStripButton1.Text = "[    Wrong    ]";
+            this.bPassAcceptable.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.bPassAcceptable.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bPassAcceptable.Image = ((System.Drawing.Image)(resources.GetObject("bPassAcceptable.Image")));
+            this.bPassAcceptable.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bPassAcceptable.Name = "bPassAcceptable";
+            this.bPassAcceptable.Size = new System.Drawing.Size(110, 28);
+            this.bPassAcceptable.Text = "   OK   ";
+            this.bPassAcceptable.Click += new System.EventHandler(this.bPassAcceptable_Click);
             // 
-            // toolStripButton2
+            // toolStripSeparator7
             // 
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(94, 22);
-            this.toolStripButton2.Text = "[      Correct      ]";
+            this.toolStripSeparator7.Name = "toolStripSeparator7";
+            this.toolStripSeparator7.Size = new System.Drawing.Size(6, 31);
             // 
-            // toolStripButton3
+            // tbComment
             // 
-            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(84, 22);
-            this.toolStripButton3.Text = "[ Acceptable ]";
+            this.tbComment.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbComment.Name = "tbComment";
+            this.tbComment.Size = new System.Drawing.Size(170, 31);
+            // 
+            // toolStripSeparator6
+            // 
+            this.toolStripSeparator6.Name = "toolStripSeparator6";
+            this.toolStripSeparator6.Size = new System.Drawing.Size(6, 31);
+            // 
+            // bFail
+            // 
+            this.bFail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.bFail.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bFail.Image = ((System.Drawing.Image)(resources.GetObject("bFail.Image")));
+            this.bFail.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bFail.Name = "bFail";
+            this.bFail.Size = new System.Drawing.Size(110, 28);
+            this.bFail.Text = "  Fail  ";
+            this.bFail.Click += new System.EventHandler(this.bFail_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 31);
+            // 
+            // bHaltTests
+            // 
+            this.bHaltTests.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.bHaltTests.BackColor = System.Drawing.Color.Red;
+            this.bHaltTests.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bHaltTests.ForeColor = System.Drawing.Color.White;
+            this.bHaltTests.Image = ((System.Drawing.Image)(resources.GetObject("bHaltTests.Image")));
+            this.bHaltTests.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bHaltTests.Name = "bHaltTests";
+            this.bHaltTests.Size = new System.Drawing.Size(62, 28);
+            this.bHaltTests.Text = "Halt";
+            this.bHaltTests.Click += new System.EventHandler(this.bHaltTests_Click);
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 31);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 31);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lbStatus,
+            this.toolStripSeparator2,
+            this.lbFilename,
+            this.lbPageNum});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(930, 25);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(38, 22);
+            this.lbStatus.Text = "status";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // lbFilename
+            // 
+            this.lbFilename.Name = "lbFilename";
+            this.lbFilename.Size = new System.Drawing.Size(53, 22);
+            this.lbFilename.Text = "filename";
+            // 
+            // lbPageNum
+            // 
+            this.lbPageNum.Name = "lbPageNum";
+            this.lbPageNum.Size = new System.Drawing.Size(13, 22);
+            this.lbPageNum.Text = "1";
+            // 
+            // pbPage
+            // 
+            this.pbPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(255)))));
+            this.pbPage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbPage.Location = new System.Drawing.Point(0, 25);
+            this.pbPage.Name = "pbPage";
+            this.pbPage.Size = new System.Drawing.Size(930, 765);
+            this.pbPage.TabIndex = 3;
+            this.pbPage.TabStop = false;
             // 
             // LayoutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 725);
+            this.ClientSize = new System.Drawing.Size(930, 821);
+            this.Controls.Add(this.pbPage);
+            this.Controls.Add(this.toolStrip2);
             this.Controls.Add(this.toolStrip1);
             this.Name = "LayoutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LayoutForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,9 +252,25 @@
 
         #endregion
 
+        private System.Windows.Forms.ToolStripButton bPass;
+        private System.Windows.Forms.ToolStripButton bIgnore;
+        private System.Windows.Forms.ToolStripButton bClearData;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton toolStripButton2;
-        private System.Windows.Forms.ToolStripButton toolStripButton3;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripLabel lbStatus;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel lbFilename;
+        private System.Windows.Forms.ToolStripLabel lbPageNum;
+        private System.Windows.Forms.ToolStripButton bFail;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripTextBox tbComment;
+        private System.Windows.Forms.ToolStripButton bHaltTests;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.PictureBox pbPage;
+        private System.Windows.Forms.ToolStripButton bPassAcceptable;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator6;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator7;
     }
 }
---
+++ Test/Render/Layout/PdfLayoutAccuracy.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using BookReader.Render;
+using BookReader.Render.Layout;
+using System.Drawing;
+using BookReader.Model;
+using BookReaderTest.TestUtils;
+using BookReader.Utils;
+using System.Drawing.Imaging;
+using PDFLibNet;
+using BookReader.Render.BookFormats;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace BookReaderTest.Render.Layout
+{
+    [TestFixture]
+    public class PdfLayoutAccuracy
+    {
+        bool _haltTests = false;
+        LayoutForm _form = new LayoutForm();
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            _haltTests = false;
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            LayoutTestCase.Save();
+        }
+
+        PDFWrapper GetPdfWrapper(IBookContent bc)
+        {
+            return ((PdfBookProvider)bc.BookProvider.o).InternalPdfWrapper.o;
+        }
+
+        DW<IBookContent> _bookContent;
+
+        [TestCaseSource("GetTestCases")]
+        public void Create(LayoutTestCase tcase)
+        {
+            if (_haltTests) { Assert.Ignore("Halt tests"); }
+
+            if (_bookContent == null || _bookContent.o.Book.Filename != tcase.Filename)
+            {
+                if (_bookContent != null) 
+                {
+                    _bookContent.DisposeItem();
+                    _bookContent = null;
+                }
+
+                Book book = new Book(tcase.Filename);
+                _bookContent = DW.Wrap<IBookContent>(new PdfBookContent(book, null));
+            }
+
+            IPageLayoutStrategy alg = new PdfWordsLayoutStrategy();
+            PageLayout layout = alg.DetectLayoutFromBook(_bookContent.o, tcase.PageNum);
+            layout.SetPageSizeToScreen(600);
+
+            // Get staus based on the layout
+            TestCaseStatus status = tcase.GetStatus(layout);
+            
+            // Skip pages that pass the test
+            if (status == TestCaseStatus.Pass_Good ||
+                status == TestCaseStatus.Pass_Acceptable) 
+            { 
+                return; 
+            }
+
+            DW<Bitmap> page = DW.Wrap(_bookContent.o.BookProvider.o.RenderPageImage(tcase.PageNum, layout.PageSize));
+            DW<Bitmap> newPage = layout.Debug_DrawLayout(page);
+
+            TestCaseStatus newStatus = _form.Show(status, tcase, newPage);
+
+            page.DisposeItem();
+            newPage.DisposeItem();
+
+            if (newStatus == TestCaseStatus.HaltTest)
+            {
+                _haltTests = true;
+                Assert.Ignore("Halt tests");                
+            }
+
+            // Update test case object
+            tcase.Comment = _form.Comment;
+
+            if (newStatus == TestCaseStatus.Pass_Acceptable ||
+                newStatus == TestCaseStatus.Pass_Good)
+            {
+                tcase.ExpectedLayout = layout;
+                tcase.ExpectedLayoutAccurate = (newStatus == TestCaseStatus.Pass_Good);
+            }
+
+            if (newStatus == TestCaseStatus.Ignore_Clear)
+            {
+                tcase.ExpectedLayout = null;
+            }
+
+            switch (newStatus)
+            {
+                case TestCaseStatus.Fail:
+                    Assert.Fail("Failed: " + tcase.Comment);
+                    break;
+                case TestCaseStatus.Ignore:
+                case TestCaseStatus.Ignore_Clear:
+                    Assert.Ignore("Ignore: " + tcase.Comment);
+                    break;
+                case TestCaseStatus.Unknown:
+                    Assert.Inconclusive();
+                    break;
+            }
+        }
+
+
+        IEnumerable<LayoutTestCase> GetTestCases()
+        {
+            const int StartCount = 7;
+            const int MidCount = 4;
+            const int EndCount = 4;
+
+            var cases = new List<LayoutTestCase>();
+
+            foreach (var file in TestConst.GetAllPdfFiles())
+            {
+                int pageCount;
+
+                Book book = new Book(file);
+                DW<IBookContent> bookC = RenderFactory.Default.NewBookContent(book, null);
+                pageCount = bookC.o.PageCount;
+                bookC.DisposeItem();
+
+                // indexes
+                var pages = LinqExtensions.IntRange(1, StartCount)
+                    .Concat(LinqExtensions.IntRange((pageCount - MidCount) / 2, MidCount))
+                    .Concat(LinqExtensions.IntRange(pageCount - EndCount + 1, EndCount))
+                    .Where(x => 1 <= x && x <= pageCount)
+                    .Distinct()
+                    .OrderBy(x => x);
+
+                foreach (int pageNum in pages)
+                {
+                    var tcase = LayoutTestCase.Get(file, pageNum);
+                    yield return tcase;
+                }
+            }
+        }
+    }
+
+    [DataContract]
+    public class LayoutTestCase 
+    {
+        [DataMember]
+        public String Filename { get; private set; }
+        
+        [DataMember]
+        public int PageNum { get; private set; }
+
+        [DataMember]
+        internal PageLayout ExpectedLayout { get; set; }
+
+        [DataMember(Name = "Accurate")]
+        public bool ExpectedLayoutAccurate { get; set; }
+
+        [DataMember]
+        public String Comment { get; set; }
+
+        LayoutTestCase(String filename, int pageNum)
+        {
+            Filename = filename;
+            PageNum = pageNum;
+        }
+
+        #region load/save
+        const string CaseFile = "LayoutTestCaseResults.xml";
+        static Dictionary<String, LayoutTestCase> _caseResults;
+        static Dictionary<String, LayoutTestCase> CaseResults 
+        {
+            get 
+            {
+                if (_caseResults == null)
+                {
+                    _caseResults = XmlHelper.DeserializeOrDefault(CaseFile, new Dictionary<string, LayoutTestCase>());
+                }
+                return _caseResults;
+            }
+        }
+
+        public static void Save()
+        {
+            XmlHelper.Serialize(CaseResults, CaseFile);
+        }
+        #endregion
+
+        public static LayoutTestCase Get(String filename, int pageNum)
+        {
+            String key = filename + "_p" + pageNum;
+
+            LayoutTestCase tcase;
+            if (!CaseResults.TryGetValue(key, out tcase))
+            {
+                tcase = new LayoutTestCase(filename, pageNum);
+                CaseResults.Add(key, tcase);
+            }
+
+            return tcase;
+        }
+
+        public override string ToString()
+        {
+            return Path.GetFileNameWithoutExtension(Filename) + " p" + PageNum;
+        }
+
+        internal TestCaseStatus GetStatus(PageLayout layout)
+        {
+            if (ExpectedLayout == null) { return TestCaseStatus.Unknown; }
+
+            // Shallow comparison, just content bounds for now
+            if (ExpectedLayout.UnitBounds.AlmostEquals(layout.UnitBounds, 0.003f)) { return TestCaseStatus.Pass_Good; }
+
+            return TestCaseStatus.Fail;
+        }
+    }
+
+    public enum TestCaseStatus
+    {
+        Unknown,
+        Pass_Good,
+        Pass_Acceptable,
+        Fail,
+        Ignore,
+        Ignore_Clear,
+        
+        HaltTest,
+    }
+}
