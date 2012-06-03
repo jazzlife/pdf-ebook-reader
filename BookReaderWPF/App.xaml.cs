97e829abc84fc5cbb8b9ca8a64998e037b7aee06
danilom <danilom@c21f06b1-db24-b691-a423-4f2667294ccf>
2012-06-03 16:09:36 +0000

    WPF project

---
+++ BookReaderWPF/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using BookReader.ViewModel;
+
+namespace BookReader
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+
+            // Create the ViewModel
+            var viewModel = new MainWindowViewModel();
+            window.DataContext = viewModel;
+
+            // Save the options when window is closed
+            window.Closed += ((src,ea) => viewModel.Close());
+
+            window.Show();
+        }
+    }
+}
---
+++ BookReaderWPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using BookReader.Base.ViewModel;
+
+namespace BookReader.ViewModel
+{
+    /// <summary>
+    /// Bind window size/position/state to settings, 
+    /// ensure they fit within the desktop.
+    /// </summary>
+    class MainWindowViewModel : ViewModelBase
+    {
+        public WindowSettings Settings { get; private set; }
+        
+        public MainWindowViewModel()
+        {
+            Settings = new WindowSettings();   
+        }
+
+        public void Close()
+        {
+            Settings.Save();
+        }
+    }
+}
